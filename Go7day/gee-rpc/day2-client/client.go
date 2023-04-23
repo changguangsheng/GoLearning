5e58320ab2b9e0de24a014915f7a2dfc875b9e82
cgs <15993713602@163.com>
2023-04-23 11:59:52 +0800

    合并master

--- Go7day/gee-rpc/day2-client/client.go
+++ Go7day/gee-rpc/day2-client/client.go
@@ -1,11 +1,20 @@
 package day2_client
 
 import (
+	"errors"
 	"geerpc/codec"
 	"io"
 	"sync"
 )
 
+/*
+cc 是消息的编解码器，和服务端类似，用来序列化将要发送出去的请求，以及反序列化接收到的响应。
+sending 是一个互斥锁，和服务端类似，为了保证请求的有序发送，即防止出现多个请求报文混淆。
+header 是每个请求的消息头，header 只有在请求发送时才需要，而请求发送是互斥的，因此每个客户端只需要一个，声明在 Client 结构体中可以复用。
+seq 用于给发送的请求编号，每个请求拥有唯一编号。
+pending 存储未处理完的请求，键是编号，值是 Call 实例。
+closing 和 shutdown 任意一个值置为 true，则表示 Client 处于不可用的状态，但有些许的差别，closing 是用户主动关闭的，即调用 Close 方法，而 shutdown 置为 true 一般是有错误发生。
+*/
 type Call struct {
 	Seq           uint64
 	ServiceMethod string
@@ -23,6 +32,7 @@ type Client struct {
 	opt      *Option
 	sending  sync.Mutex
 	header   codec.Header
+	mu       sync.Mutex
 	seq      uint64
 	pending  map[uint64]*Call
 	closing  bool
@@ -31,4 +41,29 @@ type Client struct {
 
 var _ io.Closer = (*Client)(nil)
 
-var 
+var ErrShutdown = errors.New("connection is shut down")
+
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing {
+		return ErrShutdown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
+}
+
+func (client *Client) registerCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+	
+}
